c655180a3f162b5b4842a67943dfb899a3f89237
Vitalii Rabchebskyi <carterjames@ukr.net>
2025-05-02 16:09:00 +0300

    Added tabs to Cases component

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import contentfulClient from "../contentfulConfig.js";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
-import LocomotiveScroll from "locomotive-scroll";
 
 const Cases = () => {
   const [cases, setCases] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const scrollRef = useRef(null);
+  const [activeTab, setActiveTab] = useState("tab1"); // State for active tab
 
   useEffect(() => {
     const fetchCases = async () => {
@@ -27,23 +25,6 @@ const Cases = () => {
     fetchCases();
   }, []);
 
-  useEffect(() => {
-    if (!scrollRef.current) return;
-
-    // Initialize Locomotive Scroll
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-      lerp: 0.5, // Adjust for smoothness
-    });
-
-    scroll.update();
-
-    return () => {
-      scroll.destroy(); // Clean up on component unmount
-    };
-  }, [cases]);
-
   if (loading) {
     return <p>Loading cases...</p>;
   }
@@ -55,47 +36,104 @@ const Cases = () => {
   return (
     <section className="cases" id="cases">
       <div className="container">
-        <div className="toaster">
-          <span>Helping clients achieve success</span>
+        <div className="cases__header">
+          <div className="toaster">
+            <span>Helping clients achieve success</span>
+          </div>
+          <h2>Latest cases</h2>
         </div>
-        <h2>Latest cases</h2>
-        <div className="cases__content">
-          {cases.map((item) => (
-            <div
-              key={item.sys.id}
-              className="case__body"
-              ref={scrollRef}
-              data-scroll-container
-            >
-              <div className="case__card">
-                <img
-                  src={item.fields.casesImage.fields.file.url}
-                  alt={item.fields.caseTitle}
-                />
-              </div>
-              <div className="case__card">
-                <h3>{item.fields.caseTitle}</h3>
-                <div>{documentToReactComponents(item.fields.caseBrief)}</div>
-                <div className="case__card-content">
-                  <span>Client</span>
-                  <span>{item.fields.caseClient}</span>
-                </div>
-                <div className="divider"></div>
-                <div className="case__card-content">
-                  <span>Role</span>
-                  <span>{item.fields.role}</span>
-                </div>
-                <div className="divider"></div>
-                <div className="case__tags">
-                  {item.fields.tags.map((tag, index) => (
-                    <span key={index} className="case__tag">
-                      {tag}
-                    </span>
-                  ))}
-                </div>
+        <ul className="tabs">
+          <li
+            className={activeTab === "tab1" ? "active" : ""}
+            onClick={() => setActiveTab("tab1")}
+          >
+            Websites
+          </li>
+          <li
+            className={activeTab === "tab2" ? "active" : ""}
+            onClick={() => setActiveTab("tab2")}
+          >
+            Mobile apps
+          </li>
+          <li
+            className={activeTab === "tab3" ? "active" : ""}
+            onClick={() => setActiveTab("tab3")}
+          >
+            Web apps
+          </li>
+          <li
+            className={activeTab === "tab4" ? "active" : ""}
+            onClick={() => setActiveTab("tab4")}
+          >
+            Web Design
+          </li>
+          <li
+            className={activeTab === "tab5" ? "active" : ""}
+            onClick={() => setActiveTab("tab5")}
+          >
+            Systems
+          </li>
+        </ul>
+        <div className="tab_container">
+          {activeTab === "tab1" && (
+            <div id="tab1" className="tab_content">
+              <div className="cases__content">
+                {cases.map((item) => (
+                  <div key={item.sys.id} className="case__body">
+                    <div className="case__card">
+                      <img
+                        src={item.fields.casesImage.fields.file.url}
+                        alt={item.fields.caseTitle}
+                      />
+                    </div>
+                    <div className="case__card">
+                      <h3>{item.fields.caseTitle}</h3>
+                      <div>
+                        {documentToReactComponents(item.fields.caseBrief)}
+                      </div>
+                      <div className="case__card-content">
+                        <span>Client</span>
+                        <span>{item.fields.caseClient}</span>
+                      </div>
+                      <div className="divider"></div>
+                      <div className="case__card-content">
+                        <span>Role</span>
+                        <span>{item.fields.role}</span>
+                      </div>
+                      <div className="divider"></div>
+                      <div className="case__tags">
+                        {item.fields.tags.map((tag, index) => (
+                          <span key={index} className="case__tag">
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
+          )}
+          {activeTab === "tab2" && (
+            <div id="tab2" className="tab_content">
+              <h2>Mobile apps</h2>
+            </div>
+          )}
+          {activeTab === "tab3" && (
+            <div id="tab3" className="tab_content">
+              <h2>Web apps</h2>
+            </div>
+          )}
+          {activeTab === "tab4" && (
+            <div id="tab4" className="tab_content">
+              <h2>Web design</h2>
+            </div>
+          )}
+          {activeTab === "tab5" && (
+            <div id="tab4" className="tab_content">
+              <h2>Systems</h2>
+            </div>
+          )}
         </div>
       </div>
     </section>
